8e37be1a3de5e09002e842d25e88484c201c24c6
Dmitry <dmitrbah@mail.ru>
2023-09-23 04:45:40 +0300

    updated task 009

--- Task009/Program.cs
+++ Task009/Program.cs
@@ -1,7 +1,23 @@
-﻿int number = new Random().Next(10, 100);
-Console.WriteLine($"Случайное число из отрезка (10, 99) -> {number}");
+﻿int number = new Random().Next(10, 100); // max = 99 + 1
+Console.WriteLine($"Cлучайное число из отрезка [10, 99] -> {number}");
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+// int firstDigit = number / 10; // 78 / 10 = 7
+// int secondDigit = number % 10;
+
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; // Тернарный оператор
+// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10; // 78 / 10 = 7
+    int secondDigit = num % 10;
+    int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return maxDigit;
+}
+
