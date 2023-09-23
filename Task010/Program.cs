8935e004dbc9a0dce8f022b363f57367a72fdada
Dmitry <dmitrbah@mail.ru>
2023-09-23 04:56:21 +0300

    added task 010

---
+++ Task010/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число -> {number}");
+int firstDigit = number / 100;
+int lastDigit = number % 10;
+int result = firstDigit * 10 + lastDigit;
+Console.WriteLine($"{result}");
