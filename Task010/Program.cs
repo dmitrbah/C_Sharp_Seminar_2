a9303f62bf2198d3e85d4c8b149c8da77ebdb5c6
Dmitry <dmitrbah@mail.ru>
2023-09-24 04:53:37 +0300

    Updated Task010

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -1,6 +1,15 @@
 ﻿int number = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное число -> {number}");
-int firstDigit = number / 100;
-int lastDigit = number % 10;
-int result = firstDigit * 10 + lastDigit;
-Console.WriteLine($"{result}");
+// int firstDigit = number / 100;
+// int lastDigit = number % 10;
+// int result = firstDigit * 10 + lastDigit;
+// Console.WriteLine($"{result}");
+
+int DeleteSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    int result = firstDigit * 10 + lastDigit;
+    return result;
+}
+Console.WriteLine($"{DeleteSecondDigit(number)}");
