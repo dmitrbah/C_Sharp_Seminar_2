8f060e7db9faa860f6c5c2c635033f81936a4f1f
Dmitry <dmitrbah@mail.ru>
2023-09-24 04:33:51 +0300

    Added Task 014

---
+++ Task014/Program.cs
@@ -0,0 +1,11 @@
+﻿bool MultipalTwoNumbers(int num)
+{
+bool multipal = (num % 7 == 0) && (num % 23 == 0);
+return multipal;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (MultipalTwoNumbers(number) == true) Console.WriteLine("кратно 7 и 23");
+else Console.WriteLine("не кратно 7 и 23");
