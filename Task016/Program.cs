e11cbd7a21cfe60cd8c01b74379be84e75271197
Dmitry <dmitrbah@mail.ru>
2023-09-24 04:54:05 +0300

    Updated Tak016

---
+++ Task016/Program.cs
@@ -0,0 +1,12 @@
+﻿bool SquareOfTwoNumbers(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = SquareOfTwoNumbers(number1, number2);
+Console.WriteLine(result ? "да" : "нет");
