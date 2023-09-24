efcca82aefdcaf4858deec9145234765a18bce68
Dmitry <dmitrbah@mail.ru>
2023-09-24 04:33:26 +0300

    Added Task 012

---
+++ Task012/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int Remainder(int FirstPar, int SecondPar)
+{
+    return FirstPar % SecondPar;
+}
+
+int remainder = Remainder(number1, number2);
+if (remainder == 0) Console.WriteLine("кратно"); 
+else Console.WriteLine($"не кратно, остаток равен {remainder}");
+
